ee0e50754cb5bf5943d4f16508725b3f65931f24
lixiang_yewu <lixiang_yewu@cmss.chinamobile.com>
2024-01-02 02:27:55 +0000

    update src/common/dim_baseline.c.
    Signed-off-by: lixiang_yewu <lixiang_yewu@cmss.chinamobile.com>
    
    update src/common/dim_baseline.c.
    
    Signed-off-by: lixiang_yewu <lixiang_yewu@cmss.chinamobile.com>

--- src/common/dim_baseline.c
+++ src/common/dim_baseline.c
@@ -104,7 +104,8 @@ int dim_baseline_add(struct dim_baseline_tree *root, const char *name,
 	if (ret < 0)
 		goto err;
 
-	strcpy((char *)baseline->name, name);
+        strncpy((char *)baseline->name, name, buf_len - 1);
+        baseline->name[buf_len - 1] = '\0';
 
 	write_lock(&root->lock);
 	ret = dim_baseline_rb_add(&root->rb_root, baseline, NULL);
